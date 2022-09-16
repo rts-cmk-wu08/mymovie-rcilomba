8399e6d80acffc341843ed8de5efead9ec1fe6da
Ramadan Cilomba <rcilomba@yahoo.com>
2022-09-16 11:05:52 +0200

    creating variables for header, main and footer and append them on the site

--- script/index.js
+++ script/index.js
@@ -2,4 +2,25 @@ document.addEventListener("DOMContentLoaded", () =>{
 
     let baseURL = "https://api.themoviedb.org/3";
     let apikey = "d6efc3cfd36ae94f20eb77ed991667a4";
+
+    let wrapperElm = document.querySelector(".wrapper");
+    
+
+    //creating elements, main etc and append them on the site
+    let headerElm = document.createElement("header");
+    headerElm.classList.add("header");
+    wrapperElm.append(headerElm);
+
+    let mainElm = document.createElement("main");
+    wrapperElm.append(mainElm);
+
+    let footerElm = document.createElement("footer");
+    wrapperElm.append(footerElm);
+
+    // content in the variables above
+
+    //content in header section
+    let 
+
+
 })
