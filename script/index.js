4c457174400df05cfcc6da4d54cf35798af01808
Ramadan Cilomba <rcilomba@yahoo.com>
2022-09-18 10:34:17 +0200

    adding elements in created article

--- script/index.js
+++ script/index.js
@@ -20,7 +20,49 @@ document.addEventListener("DOMContentLoaded", () =>{
     // content in the variables above
 
     //content in header section
-    let 
+    headerElm.innerHTML=`
+    <h1>MyMovies</h1>
+    <button>switch</button>
+    `
 
+    // now showing logic here
+    let popularElm = document.createElement("section");
+    popularElm.classList.add("popular");
+    mainElm.append(popularElm);
 
+    let popularHeader = document.createElement("header")
+     popularHeader.innerHTML=`
+    <h2>Popular</h2>
+    <a href="#">Show more</a>
+    
+   
+    `
+    popularElm.append(popularHeader);
+
+    let popularMovies = document.createElement("div");
+    popularElm.append(popularMovies)
+
+    fetch(`${baseURL}/movie/popular?api_key=${apikey}`)
+    .then(response => response.json())
+    .then(data => {
+
+        console.log(data.results[0])
+        
+        
+        data.results.forEach(movie => {
+            let article = document.createElement("article")
+            article.classList.add("movie-article")
+            article.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} poster">
+            <div>
+                <h3>${movie.title}</h3>
+                <p>${movie.vote_average}/10 IMDB</p>
+                <P class="genres"></p>
+        </div>
+
+            
+            `
+
+            popularMovies.append(article)
+        })
+    })
 })
