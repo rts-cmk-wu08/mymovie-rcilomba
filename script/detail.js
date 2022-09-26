08352eb86f084e24bbc53a42775dcec27a37dde8
Ramadan Cilomba <rcilomba@yahoo.com>
2022-09-26 14:56:05 +0200

    adding img gor cast section by adding for eachEach and adding member and index as parameters

--- script/detail.js
+++ script/detail.js
@@ -1,48 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let params = new URLSearchParams(window.location.search);
+  let id = params.get("id");
 
-    let params = new URLSearchParams(window.location.search);
-    let id = params.get("id");
+  let baseURL = "https://api.themoviedb.org/3";
+  let apikey = "d6efc3cfd36ae94f20eb77ed991667a4";
 
-    let baseURL ="https://api.themoviedb.org/3";
-    let apikey ="d6efc3cfd36ae94f20eb77ed991667a4";
+  console.log(id);
+  // console.log("test" + id);
 
-    console.log(id)
-    // console.log("test" + id);
+  let wrapperElm = document.querySelector(".wrapper");
 
-    let wrapperElm = document.querySelector(".wrapper");
+  //createing elements, main etc and append them on the detail site
+  let headerElm = document.createElement("header");
+  // headerElm.classList.add("movieHeader");
 
+  wrapperElm.append(headerElm);
 
-        //createing elements, main etc and append them on the detail site
-    let headerElm = document.createElement("header");
-    // headerElm.classList.add("movieHeader");
-    
-    wrapperElm.append(headerElm);
+  let mainElm = document.createElement("main");
+  wrapperElm.append(mainElm);
 
-    let mainElm = document.createElement("main");
-    wrapperElm.append(mainElm);
+  let footerElm = document.createElement("footer");
+  wrapperElm.append(footerElm);
 
-    let footerElm = document.createElement("footer");
-    wrapperElm.append(footerElm);
+  //skapa en fetch till video?
+  //https://developers.themoviedb.org/3/movies/get-movie-videos
 
-    //skapa en fetch till video?
-    //https://developers.themoviedb.org/3/movies/get-movie-videos
+  // testar
+  fetch(`${baseURL}/movie/${id}?api_key=${apikey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data)
 
-    
-
-    // testar
-    fetch(`${baseURL}/movie/${id}?api_key=${apikey}`)
-    .then(response => response.json())
-    .then(data => {
-
-        // console.log(data)
-
-       
-        // test forEach
-        // data.results.forEach(movie => {
-
-    let movieHeader =document.createElement("div");
-    movieHeader.classList.add("movieHeader");
-    movieHeader.innerHTML=`
+      let movieHeader = document.createElement("div");
+      movieHeader.classList.add("movieHeader");
+      movieHeader.innerHTML = `
     <div class="movieHeader__container">
          <div class="movieHeader__containerBox">
             <a href="index.html"><i class="fa-solid fa-arrow-left"></i></a>
@@ -57,15 +48,14 @@ document.addEventListener("DOMContentLoaded", () => {
     </div>
     <!-- ersätt img med video trailer istället  -->
     <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${data.title} poster">
-    `
-    headerElm.append(movieHeader);
+    `;
+      headerElm.append(movieHeader);
 
-        // fetch for length, language and rating
- 
-        
-    let movieTitle = document.createElement("div");
-    movieTitle.classList.add("movieTitle");
-    movieTitle.innerHTML=`
+      // fetch for length, language and rating
+
+      let movieTitle = document.createElement("div");
+      movieTitle.classList.add("movieTitle");
+      movieTitle.innerHTML = `
     <div class="movieTitleStart">
         <div class="movieTitleStartBox">
         <h1>${data.title}</h1>
@@ -99,95 +89,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
     </div>
     
-    `
-    
-    mainElm.append(movieTitle);
+    `;
 
-    let descriptionSection = document.createElement("section");
-    descriptionSection.classList.add("description");
-    descriptionSection.innerHTML=`
+      mainElm.append(movieTitle);
+
+      let descriptionSection = document.createElement("section");
+      descriptionSection.classList.add("description");
+      descriptionSection.innerHTML = `
     <h2>Description</h2>
          <p>${data.overview}</p>
-    `
-    mainElm.append(descriptionSection);
+    `;
+      mainElm.append(descriptionSection);
 
+      // skapa en fetch metod för att få fram api för cast
 
+      fetch(`${baseURL}/movie/${id}/credits?api_key=${apikey}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.cast);
 
-    
+          data.cast.forEach((member, index) => {
+            let castElm = document.createElement("div");
+            castElm.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500/${member.profile_path}" alt="">
+            <p>${member.original_name}</p>
+            `;
 
-    // skapa en fetch metod för att få fram api för cast
+            // footerElm.append(castElm);
 
-    fetch(`${baseURL}/movie/${id}/credits?api_key=${apikey}`)
-    .then(response => response.json())
-    .then (data => {
-
-        console.log("2" + data)
-    
-
-    let cast = document.createElement("div");
-    cast.innerHTML=`
-    <div class="castHeader__container">
-        <div class="castHeader__containerBox">
-        <h2>Cast</h2>
-    </div>
-    <div class="castHeader__containerBox">
-        <a href="">See more</a>    
-    </div>
-
-    </div>
-
-  
-   
-<!-- cast  -->
-
-        
-    <div class="cast__container">
-        <div class="cast__containerBox">
-            <img src="https://image.tmdb.org/t/p/w500${data.id}" alt="${data.title} poster">
-            <p>${data.original_name}</p>
-    </div>
-
-    <div class="cast__containerBox">
-<img src="" alt="${data.title} poster">
-<p></p> 
-    </div>
-
-    <div class="cast__containerBox">
-<img src="" alt="${data.title} poster">
-<p></p>
-    </div>
-
-    <div class="cast__containerBox">
-<img src="" alt="${data.title} poster">
-<p></p>
-    </div>
-    
-
-    </div>
-    `
-
-    footerElm.append(cast);
-
-
-    })
-    
-    
-
-    
-        })
-
-    })
-
- 
-
-
-    
-
-    
-
-    
-    
-
-
-
-
+            // if statement if cast is more than 4
+            if (index < 4) {
+              footerElm.append(castElm);
+            }
+          });
+        });
+    });
+});
--- script/index.js
+++ script/index.js
@@ -1,37 +1,29 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    console.log(genres)
-    let baseURL = "https://api.themoviedb.org/3";
-    let apikey = "d6efc3cfd36ae94f20eb77ed991667a4";
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(genres);
+  let baseURL = "https://api.themoviedb.org/3";
+  let apikey = "d6efc3cfd36ae94f20eb77ed991667a4";
 
-    let wrapperElm = document.querySelector(".wrapper");
+  let wrapperElm = document.querySelector(".wrapper");
 
-    
-    
+  //creating elements, main etc and append them on the site
+  let headerElm = document.createElement("header");
+  headerElm.classList.add("header");
+  wrapperElm.append(headerElm);
 
-    //creating elements, main etc and append them on the site
-    let headerElm = document.createElement("header");
-    headerElm.classList.add("header");
-    wrapperElm.append(headerElm);
+  let mainElm = document.createElement("main");
+  wrapperElm.append(mainElm);
 
-    let mainElm = document.createElement("main");
-    wrapperElm.append(mainElm);
+  let footerElm = document.createElement("footer");
+  wrapperElm.append(footerElm);
 
-    let footerElm = document.createElement("footer");
-    wrapperElm.append(footerElm);
+  let myMoviesElm = document.createElement("div");
+  myMoviesElm.classList.add("myMovies");
+  mainElm.append(myMoviesElm);
 
-    let myMoviesElm = document.createElement("div");
-    myMoviesElm.classList.add("myMovies")
-    mainElm.append(myMoviesElm);
+  // content in the variables above
 
-
-
-    // content in the variables above
-
-   
-
-
-    //content in header section
-    headerElm.innerHTML=`
+  //content in header section
+  headerElm.innerHTML = `
 <div class="moviesHeader">
 <h1>MyMovies</h1>
     <!-- <button>switch</button> -->
@@ -47,128 +39,101 @@ document.addEventListener("DOMContentLoaded", () =>{
     <a href="#">See more</a>
 </div>
     
-    `
+    `;
 
-   
+  // now showing logic here
+  let nowShowingElm = document.createElement("section");
+  nowShowingElm.classList.add("nowShowing");
 
+  mainElm.append(nowShowingElm);
 
-    // now showing logic here
-    let nowShowingElm = document.createElement("section");
-    nowShowingElm.classList.add("nowShowing");
-    
-    mainElm.append(nowShowingElm);
+  //fetch metod för att få fram API för now showing
+  fetch(`${baseURL}/movie/now_playing?api_key=${apikey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((movie) => {
+        let nowShowing = document.createElement("div");
+        // adding link
+        // nowShowing.setAttribute("href", `detail.html?id=${movie.title}`)
 
-    
-
-
-    //fetch metod för att få fram API för now showing
-    fetch(`${baseURL}/movie/now_playing?api_key=${apikey}`)
-    .then(response => response.json())
-    .then(data => {
-
-   
-
-    data.results.forEach(movie => {
-
-let nowShowing = document.createElement("div");
-// adding link
-    // nowShowing.setAttribute("href", `detail.html?id=${movie.title}`)
-
-    nowShowing.classList.add("nowShowing__container");
-        nowShowing.innerHTML=`
+        nowShowing.classList.add("nowShowing__container");
+        nowShowing.innerHTML = `
          
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} poster">
-        <p class="">${movie.title}</p>
+        <a href="">${movie.title}</a>
         <p><i class="fa-sharp fa-solid fa-star"></i>${movie.vote_average}/10 IMDB</p>
-        `
+        `;
 
-        myMoviesElm.append(nowShowing)
-        
-    })
-    
-    }) 
+        myMoviesElm.append(nowShowing);
+      });
+    });
 
+  mainElm.append(nowShowingElm);
 
-    mainElm.append(nowShowingElm);
+  //popular showing
+  let popularElm = document.createElement("section");
+  popularElm.classList.add("popular");
+  mainElm.append(popularElm);
 
-
-
-    //popular showing
-    let popularElm = document.createElement("section");
-    popularElm.classList.add("popular");
-    mainElm.append(popularElm);
-
-    let popularHeader = document.createElement("header")
-     popularHeader.innerHTML=`
+  let popularHeader = document.createElement("header");
+  popularHeader.innerHTML = `
      <div class="popular-seeMore">
     <h2>Popular</h2>
     <a href="#">See more</a>
 </div>
    
-    `
-    popularElm.append(popularHeader);
+    `;
+  popularElm.append(popularHeader);
 
-   
-    
+  //polular movies showing here
+  let popularMovies = document.createElement("div");
 
+  popularMovies.classList.add("pouplarmovies__container");
+  popularElm.append(popularMovies);
 
-    //polular movies showing here
-    let popularMovies = document.createElement("div");
-    
-    popularMovies.classList.add("pouplarmovies__container")
-    popularElm.append(popularMovies)
+  fetch(`${baseURL}/movie/popular?api_key=${apikey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.results[0]);
 
-    fetch(`${baseURL}/movie/popular?api_key=${apikey}`)
-    .then(response => response.json())
-    .then(data => {
+      data.results.forEach((movie) => {
+        let article = document.createElement("article");
+        // adding link
+        article.setAttribute("href", `detail.html?id=${movie.id}`);
 
-        console.log(data.results[0])
-        
-        
-        data.results.forEach(movie => {
-            let article = document.createElement("article")
-            // adding link
-    article.setAttribute("href", `detail.html?id=${movie.id}`)
-
-            article.classList.add("movie-article")
-            article.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} poster">
+        article.classList.add("movie-article");
+        article.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} poster">
 
             <div class="">
-                <h3><a href="details.html?id=${movie.id}">${movie.title}</a></h3>
-                <a class="movie-title"><i class="fa-sharp fa-solid fa-star"></i>${movie.vote_average}/10 IMDB</p>
+                <a href="details.html?id=${movie.id}">${movie.title}</a>
+                <p class="movie-title grey"><i class="fa-sharp fa-solid fa-star"></i>${movie.vote_average}/10 IMDB</p>
                 <P class="genres"></p>
                 <P class="runtimes"></p>
         </div>
 
             
-            `
+            `;
 
-            popularMovies.append(article)
+        popularMovies.append(article);
 
-            let genreElm = article.querySelector(".genres");
-            movie.genre_ids.forEach(id => {
-               
-                let currentGenre = genres.find(genre => genre.id == id)
-                console.log(currentGenre)
-                let genreSpan = document.createElement("span")
-                genreSpan.classList.add("genre__pill")
-                genreSpan.innerText = currentGenre.name
-                genreElm.append(genreSpan)
-            })
+        let genreElm = article.querySelector(".genres");
+        movie.genre_ids.forEach((id) => {
+          let currentGenre = genres.find((genre) => genre.id == id);
+          console.log(currentGenre);
+          let genreSpan = document.createElement("span");
+          genreSpan.classList.add("genre__pill");
+          genreSpan.innerText = currentGenre.name;
+          genreElm.append(genreSpan);
+        });
 
-            // footer
-            footerElm.classList.add("footer-icon")
-            footerElm.innerHTML=`
+        // footer
+        footerElm.classList.add("footer-icon");
+        footerElm.innerHTML = `
             
             <i class="fa-solid fa-film"></i>
+            <i class="fa-solid fa-ticket"></i>
             <i class="fa-regular fa-bookmark"></i>
-            <i class="fa-regular fa-bookmark"></i>
-            `
-
-
-
-
-
-        })
-    })
-})
+            `;
+      });
+    });
+});
