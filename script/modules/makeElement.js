8a52368aba7716b7a728db36f6fa2c98f1b96336
Ramadan Cilomba <rcilomba@yahoo.com>
2022-10-06 15:50:21 +0200

    adding comments in makeElement.js

--- script/modules/makeElement.js
+++ script/modules/makeElement.js
@@ -1,5 +1,5 @@
 export let makeElement = function (elmName, className) {
-  let element = document.createElement(elmName);
-  element.classList.add(className);
-  return element;
+  let element = document.createElement(elmName); // tar fatt i document.createElement som finns i index.js (elmName) er parametern som finns i functionen makeElement ovan. (elmName) = namn på element fx div
+  element.classList.add(className); //(className) namn på klassen
+  return element; // returnerar elementet
 };
